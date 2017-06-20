918b0d322ced5572d6d11e2b17bcfde8e1afd520
Dave <davemgoldsmith@yahoo.com>
2017-06-20 12:03:53 -0600

    password generator class - demonstration of JavaDoc

---
+++ src/edu/cnm/deepdive/security/PasswordGenerator.java
@@ -0,0 +1,24 @@
+/*
+ * PasswordGenerator.java
+ */
+
+package edu.cnm.deepdive.security;
+
+/**
+ * Implementation of a <em>simple</em> password generator.
+ * 
+ * @author Dave Goldsmith
+ * @version 1.0 
+ */
+public class PasswordGenerator {
+
+	/**
+	 * Test rig for generating passwords.
+	 * @param args  Command line parameters for generation options.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
