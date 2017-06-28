2bc8f13c4bfafffa7114e0c1c350082d1641c1ea
Dave <davemgoldsmith@yahoo.com>
2017-06-28 16:39:29 -0600

    VERSION ONE

--- src/edu/cnm/deepdive/security/Guard.java
+++ src/edu/cnm/deepdive/security/Guard.java
@@ -3,19 +3,12 @@
  */
 package edu.cnm.deepdive.security;
 
-
 import java.util.HashMap;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-
 /**
- * This is a program to generate random passwords and passphrases.
- * Generation uses a cryptographically secure random number generator
- * to select words from a list, or characters from a pool.
+ * This is a program to generate random passwords and passphrases. Generation
+ * uses a cryptographically secure random number generator to select words from
+ * a list, or characters from a pool.
  * 
  * 
  * @author dave goldsmith
@@ -23,107 +16,34 @@ import org.apache.commons.cli.ParseException;
 public class Guard {
 
 	/**
-	 * Pass command line arguments to a parser from the Apache Commons CLI library,
-	 * then instantiate and invoke the appropriate classes and methods to 
-	 * generate the requested artifact.
+	 * Pass command line arguments to a parser from the Apache Commons CLI
+	 * library, then instantiate and invoke the appropriate classes and methods
+	 * to generate the requested artifact.
 	 * 
-	 * @param args    Command line arguments specifying generation options.
+	 * @param args
+	 *            Command line arguments specifying generation options.
 	 */
 	public static void main(String[] args) {
-	/** get options
-	 *  generate artifact
-	 *  emit artifact
-	 */ 
-	
-	HashMap<String, Object> map = getOptions(args);
-	String artifact = generateArtifact(map);
-	emitArtifact(artifact);
-		
-	}
-	
-	static HashMap<String, Object> getOptions(String[] args) {
-			
-		try {
-			Option lengthOption = Option.builder("L").argName("length")
-													 .hasArg()
-													 .longOpt("length")
-													 .numberOfArgs(1)
-													 .optionalArg(true)
-													 .type(Number.class)													
-													 .build();
-			Option delimiterOption = Option.builder("d").argName("delimiter")
-														.hasArg()
-														.longOpt("delimiter")
-														.numberOfArgs(1)
-														.optionalArg(true)
-														.type(String.class)
-														.build();
-			Option wordListOption = Option.builder("w").argName("path-to-list-file")
-														.hasArg()
-														.longOpt("word-list")
-														.numberOfArgs(1)
-														.type(String.class)
-														.build();
-			
-			Option excludeUpperOption = Option.builder("b").longOpt("exclude-upper")
-														.hasArg(false)
-														.build();
-			Option excludeLowerOption = Option.builder("s").longOpt("exclude-lower")
-														.hasArg(false)														
-														.build();
-			Option excludeDigitsOption = Option.builder("n").longOpt("exclude-digits")
-														.hasArg(false)													
-														.build();
-			Option excludePunctuationOption = Option.builder("p").longOpt("exclude-punctuation")
-														.hasArg(false)											
-														.build();
-			Option includeAmbiguousOption = Option.builder("a").longOpt("include-ambiguous")
-														.hasArg(false)														
-														.build();
-			Option modeOption = Option.builder("m").longOpt("password-mode")
-													.hasArg(false)
-													.build();
-			
-			
-			Options opts = new Options().addOption(lengthOption)
-										.addOption(delimiterOption)
-										.addOption(wordListOption)
-										.addOption(excludeUpperOption)
-										.addOption(excludeLowerOption)
-										.addOption(excludeDigitsOption)
-										.addOption(excludePunctuationOption)
-										.addOption(includeAmbiguousOption)
-										.addOption(modeOption);
+		/**
+		 * get options generate artifact emit artifact
+		 */
 
-			DefaultParser parser = new DefaultParser();
-			HashMap<String, Object> map = new HashMap<>();
-			CommandLine cmdLine = parser.parse(opts,  args);
-			for (Option option : cmdLine.getOptions()) {
-				String opt = option.getOpt();
-				map.put(opt, cmdLine.getParsedOptionValue(opt));
-			}
-			return map;
-			
-			
-		} catch (ParseException ex) {
-			//TODO handle this exception with a usage display.
-			return null;
-		}
-	}
-	
+		HashMap<String, Object> map = Options.getOptions(args);
+		String artifact = generateArtifact(map);
+		emitArtifact(artifact);
+	}  // main
+
 	static String generateArtifact(HashMap<String, Object> map) {
 		if (map.containsKey("m")) {
 			PasswordGenerator gen = new SecurePasswordGenerator();
 			// TODO set fields for all specified options.
-		}
-		return null;  // FIXME
-	}
+		}   //if
+		return null; // FIXME
+	}  //generateArtifact
 
-	static void emitArtifact (String artifact) {
-		//TODO make this smarter
+	static void emitArtifact(String artifact) {
+		// TODO make this smarter
 		System.out.println(artifact);
-	
-		
-	}
-	
-}
+	} //emit artifact
+
+}  // Guard Class
---
+++ src/edu/cnm/deepdive/security/Options.java
@@ -0,0 +1,124 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.security;
+
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.ParseException;
+
+/**
+ * @author davem
+ *
+ */
+public class Options {
+	
+	 private static final String OPTIONS_DESCRIPTION_BUNDLE = "resources/options";
+	 private static final String HELP_OPTION_KEY = "help.option";
+	 private static final String LENGTH_OPTION_KEY = "length.option";
+	 private static final String DELIMITER_OPTION_KEY= "delimiter.option";
+	 private static final String LIST_OPTION_KEY	 ="word-list.option"; 
+	 private static final String MODE_OPTION_KEY ="mode.option";
+	 private static final String EXCLUDE_UPPER_OPTION_KEY="upper.option";
+	 private static final String EXCLUCDE_LOWER_OPTION_KEY="lower.option";
+	 private static final String EXCLUDE_DIGITS_OPTION_KEY = "digits.option"; 
+	 private static final String EXCLUDE_PUNCTUATION_OPTION_KEY = "punctuation.option";
+	 private static final String EXCLUDE_AMBIGUOUS_OPTION_KEY= "ambiguous.option";
+	
+	static HashMap<String, Object> getOptions(String[] args) {
+		
+		try {
+			ResourceBundle bundle = ResourceBundle.getBundle(OPTIONS_DESCRIPTION_BUNDLE);
+			Option helpOption = Option.builder("?").longOpt("help")					
+													.hasArg(false)
+													.desc(bundle.getString(HELP_OPTION_KEY))
+													.build();
+			
+			Option lengthOption = Option.builder("L").argName("length")
+													 .hasArg()
+													 .longOpt("length")
+													 .numberOfArgs(1)
+													 .optionalArg(true)
+													 .type(Number.class)
+													 .desc(bundle.getString(LENGTH_OPTION_KEY))
+													 .build();
+			Option delimiterOption = Option.builder("d").argName("delimiter")
+														.hasArg()
+														.longOpt("delimiter")
+														.numberOfArgs(1)
+														.optionalArg(true)
+														.type(String.class)
+														.desc(bundle.getString(DELIMITER_OPTION_KEY))
+														.build();
+			Option wordListOption = Option.builder("w").argName("path-to-list-file")
+														.hasArg()
+														.longOpt("word-list")
+														.numberOfArgs(1)
+														.type(String.class)
+														.desc(bundle.getString(LIST_OPTION_KEY))
+														.build();
+			
+			Option excludeUpperOption = Option.builder("b").longOpt("exclude-upper")
+														.hasArg(false)
+														.desc(bundle.getString(UPPER_OPTION_KEY))
+														.build();
+			Option excludeLowerOption = Option.builder("s").longOpt("exclude-lower")
+														.hasArg(false)
+														.desc(bundle.getString(LOWER_OPTION_KEY))
+														.build();
+			Option excludeDigitsOption = Option.builder("n").longOpt("exclude-digits")
+														.hasArg(false)	
+														.desc(bundle.getString(DIGITS_OPTION_KEY))
+														.build();
+			Option excludePunctuationOption = Option.builder("p").longOpt("exclude-punctuation")
+														.hasArg(false)
+														.desc(bundle.getString(PUNCTUATION_OPTION_KEY))
+														.build();
+			Option includeAmbiguousOption = Option.builder("a").longOpt("include-ambiguous")
+														.hasArg(false)
+														.desc(bundle.getString(AMBIGUOUS_OPTION_KEY))
+														.build();
+			Option modeOption = Option.builder("m").longOpt("password-mode")
+														.hasArg(false)
+														.desc(bundle.getString(MODE_OPTION_KEY))
+														.build();
+			
+			
+			org.apache.commons.cli.Options opts 
+				= new org.apache.commons.cli.Options().addOption(lengthOption)
+														.addOption(helpOption)  //Nate
+														.addOption(delimiterOption)
+														.addOption(wordListOption)
+														.addOption(excludeUpperOption)
+														.addOption(excludeLowerOption)
+														.addOption(excludeDigitsOption)
+														.addOption(excludePunctuationOption)
+														.addOption(includeAmbiguousOption)
+														.addOption(modeOption);
+
+			DefaultParser parser = new DefaultParser();
+			HashMap<String, Object> map = new HashMap<>();
+			CommandLine cmdLine = parser.parse(opts,  args);
+			if (cmdLine.hasOption('?')) {										//Nate
+				new HelpFormatter().printHelp("These are the options", opts);  //FIXME
+			}
+			for (Option option : cmdLine.getOptions()) {
+				String opt = option.getOpt();
+				map.put(opt, cmdLine.getParsedOptionValue(opt));
+			}
+			return map;
+			
+			
+		} catch (ParseException ex) {
+			//TODO handle this exception with a usage display.
+			return null;
+		}
+	}
+	
+
+}
