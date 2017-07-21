451a6412554b6e7dbf5bdd4dae7852939ee11b0c
Dave <davemgoldsmith@yahoo.com>
2017-07-21 14:23:41 -0600

    adding ui

--- src/edu/cnm/deepdive/security/GuardUi.java
+++ src/edu/cnm/deepdive/security/GuardUi.java
@@ -6,9 +6,12 @@ package edu.cnm.deepdive.security;
 import java.util.ResourceBundle;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -18,20 +21,43 @@ import javafx.stage.Stage;
 public class GuardUi extends Application {
 
 	private static final String STYLE_SHEET = "/styles/application.css";
-	private static final String LAYOUT_RESOURCE = "/layouts/Generate.fxml";
+	private static final String LAYOUTS_RESOURCE = "/layouts/Generate.fxml";
 	private static final String UI_BUNDLE = "resources/ui";
+	private static final String WINDOW_TITLE_KEY = "window.title";
 	
+	@FXML
+	private VBox passwordOptions;
+	@FXML
+	private VBox passphraseOptions;
+	@FXML
+	private CheckBox upperIncluded;
+	@FXML
+	private CheckBox lowerIncluded;
+	@FXML
+	private CheckBox punctuationIncluded;
+	@FXML
+	private CheckBox ambiguousExcluded;
+	
+	private SecurePasswordGenerator passwordGenerator = new SecurePasswordGenerator();
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javafx.application.Application#start(javafx.stage.Stage)
 	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		ResourceBundle uiBundle = ResourceBundle.getBundle(UI_BUNDLE);
-		Pane pane = FXMLLoader.load(getClass().getResource(LAYOUT_RESOURCE), uiBundle);
+		Pane pane = FXMLLoader.load(getClass().getResource(LAYOUTS_RESOURCE), uiBundle);
 		Scene scene = new Scene(pane, 400, 400);
 		scene.getStylesheets().add(getClass().getResource(STYLE_SHEET).toExternalForm());
 		primaryStage.setScene(scene);
+		primaryStage.show();
+		primaryStage.setTitle(uiBundle.getString(WINDOW_TITLE_KEY));
+		//upperIncluded.setSelected(passwordGenerator.isUpperCaseIncluded());
+		//lowerIncluded.setSelected(passwordGenerator.isLowerCaseIncluded());
+		//punctuationIncluded.setSelected(passwordGenerator.isPunctuationIncluded());
+		//ambiguousExcluded.setSelected(passwordGenerator.isAmbiguousIncluded());
 		primaryStage.show();
 	}
 
@@ -40,25 +66,7 @@ public class GuardUi extends Application {
 	 */
 	public static void main(String[] args) {
 		launch(args);
-	
 
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
